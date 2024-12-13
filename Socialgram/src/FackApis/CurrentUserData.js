7be9c585112baea863084955f81fc11ddca3d0a9
jagadeeshrayavelluri97 <rayavellurijagadeesh@gmail.com>
2024-12-13 09:33:47 +0530

    solution

---
+++ Socialgram/src/FackApis/CurrentUserData.js
@@ -0,0 +1,14 @@
+import ProfieImage from "../assets/img/CurentProfile.jpeg";
+import CoverPhoto from "../assets/img/CoverPhotos.jpg";
+
+const CurrentUser = [
+  {
+    id: "me",
+    name: "Jagadeesh",
+    username: "@thebegjoker",
+    ProfieImage: ProfieImage,
+    CoverPhoto: CoverPhoto,
+  },
+];
+
+export default CurrentUser;
